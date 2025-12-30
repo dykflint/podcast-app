fa2fa45d82df30888ab2126cba23b4a287093531
DykFlint <konstantin.tamoev@gmail.com>
2025-12-30 07:10:58 +0100

    Add new vercel app to cors

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(
   cors({
     origin: [
       'http://localhost:5173',
-      'https://podcast-app-vercel-git-main-dykflints-projects.vercel.app/',
+      'https://podcast-app-git-main-dykflints-projects.vercel.app/',
     ],
     credentials: true,
   }),
